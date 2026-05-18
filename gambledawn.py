1afdc57f6db3dab3186a8bdd9d3ca30ddcdd53a5
dawnera <ripbob121@gmail.com>
2026-05-18 13:13:57 +1000

    imports

---
+++ gambledawn.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('discord_token')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
